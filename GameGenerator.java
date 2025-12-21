ea0cc60b1a7f14be3cf4a9e8448955493b93ce38
hh4611915 <hh4611915@gmail.com>
2025-12-21 09:47:01 +0200

    update generator

--- GameGenerator.java
+++ GameGenerator.java
@@ -10,19 +10,18 @@ public class GameGenerator {
         SudokuBoard board = new SudokuBoard(solved);
         RandomPairs rp = new RandomPairs();
 
+        
         int removeCount = switch (level) {
             case EASY -> 10;
-            case MEDIUM -> 25;
-            case HARD -> 20;
+            case MEDIUM -> 20;
+            case HARD -> 25;
         };
 
         List<int[]> pairs = rp.generateDistinctPairs(removeCount);
 
+
         for (int[] p : pairs) {
-            int index = p[0] % 81;
-            int row = index / 9;
-            int col = index % 9;
-            board.setCell(row, col, 0);
+            board.setCell(p[0], p[1], 0);
         }
 
         return board;
